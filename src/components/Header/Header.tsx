e57cb4dd7f2e3175dbdca41813c4289633820143
Thaman Chand <thaman.chand@invian.fi>
2021-05-30 22:08:51 +0300

    added Header component and it's styles

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,27 @@
+import React, {useState} from 'react';
+
+import Logo from '../Logo/Logo';
+import Cart from '../Cart/Cart';
+import CartIcon from '../CartIcon/CartIcon';
+
+import './styles.scss';
+
+const Header = () => {
+    const [toggleCart, setToggleCart] = useState(false);
+
+    const handleCartToggle = () => {
+        setToggleCart(!toggleCart);
+    }
+    // Header logic
+    return (
+        <div className="header">
+            <Logo />
+           <CartIcon onClick={handleCartToggle} />
+            {toggleCart && <Cart />}
+        </div>
+    )
+
+}
+
+export default Header;
+
