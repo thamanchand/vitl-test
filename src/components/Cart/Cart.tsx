ac783f1953c63542fa39887fc1c1d9e13ed6a143
Thaman Chand <thaman.chand@invian.fi>
2021-05-31 19:12:19 +0300

    added Total component, computed total sum

--- src/components/Cart/Cart.tsx
+++ src/components/Cart/Cart.tsx
@@ -1,34 +1,37 @@
-import React from 'react';
+import React, {useContext} from 'react';
 
 import Button from '../Button/Button';
 import CartItem from "../CartItem/CartItem";
+import VitlProductContext from '../../context';
+import { productTotal } from '../../utils';
 
 import './styles.scss';
 
-const cartItems = [
-    {
-        id: 1,
-        name: 'Suppliment 1',
-        price: 20
-    },
-    {
-        id: 2,
-        name: 'Suppliment 2',
-        price: 30
-    }
-]
+const Total = ({ basket }: any) => {
+    return (
+        <>
+            <div className="totalDivider" />
+            <div className="totalSum">
+                <span className="totalLabel">Total</span>
+                <span className="totalPrice">£{productTotal(basket)}</span>
+            </div>
+        </>
+    )
+};
+
 const Cart = () => {
+    const { basket } = useContext(VitlProductContext);
+
     return (
         <div className="cartDropdown">
-            <div className="cartItems">
-                {cartItems.length ? (
-                    cartItems.map(cartItem => (
-                        <CartItem key={cartItem.id} item={cartItem} />
+            {basket.length ? (
+                basket.map((cartItem: any, index: number) => (
+                    <CartItem key={index} item={cartItem} />
                     ))
                 ) : (
                     <span className="emptyMessage">Your cart is empty</span>
-                )}
-            </div>
+            )}
+            {basket.length > 0 && <Total basket={basket} /> }
             <Button
                 onClick={() => console.log("checkout dispatched")}
             >
