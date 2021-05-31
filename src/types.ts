447116485f61eb7fcc77d043f17785c3f3fda359
Thaman Chand <thaman.chand@invian.fi>
2021-05-31 15:26:40 +0300

    added Nutrient, Product, TUL, Configs types definition

---
+++ src/types.ts
@@ -0,0 +1,24 @@
+
+export type Nutrient = {
+    id: string;
+    amount: number;
+};
+
+export type Product = {
+    name: string;
+    price: string;
+    nutrients: Nutrient[];
+
+}
+
+export type TUL = {
+    id: string;
+    amount: number;
+    unit: string;
+};
+
+
+export type Configs = {
+    tolerableUpperLimits:TUL[],
+    currency: string;
+}
