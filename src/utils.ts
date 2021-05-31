2cbe540f8a8c05a3ee32789ff3a691883b43a0f1
Thaman Chand <thaman.chand@invian.fi>
2021-05-31 23:08:50 +0300

    created getNutrientTUL and findNutrientFromBasket util

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,14 @@
+import { Configs, Nutrient } from './types';
+
 export const productTotal = (basket: any) => {
      return basket.reduce((accumulator: number, currentValue: any) => {
         return accumulator + currentValue.price;
     },0);
+}
+export const getNutrientTUL = (tolerableUpperLimits: Configs[], id: string) => {
+    return tolerableUpperLimits.find((config: any) => config.id === id);
+}
+
+export const findNutrientFromBasket = (nutrients: Nutrient[], id: string) => {
+    return nutrients.find((item: any) => item.id === id);
 }
