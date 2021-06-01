7e11f521e33f04b2ea81f843055d5f9f132c4f2d
Thaman Chand <thaman.chand@invian.fi>
2021-06-01 06:53:55 +0300

    isProductFoundInBasket util

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,4 @@
-import { Configs, Nutrient } from './types';
+import {Configs, Nutrient, Product} from './types';
 
 export const productTotal = (basket: any) => {
      return basket.reduce((accumulator: number, currentValue: any) => {
@@ -11,4 +11,8 @@ export const getNutrientTUL = (tolerableUpperLimits: Configs[], id: string) => {
 
 export const findNutrientFromBasket = (nutrients: Nutrient[], id: string) => {
     return nutrients.find((item: any) => item.id === id);
+}
+
+export const isProductFoundInBasket = (basket: Product[], name: string) => {
+    return basket.filter((item: any) => item.name === name).length < 1;
 }
