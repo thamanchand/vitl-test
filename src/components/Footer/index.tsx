e513d382990697723a1b15c35e3304a7f584338b
Thaman Chand <thaman.chand@invian.fi>
2021-06-03 13:17:12 +0300

    Created Footer component with basic text and style

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import './styles.scss';
+
+const Footer= () => {
+    return (
+        <div className="footer">
+            <p>© 2021 Vitl. All rights reserved</p>
+        </div>
+    )
+};
+export default Footer;
