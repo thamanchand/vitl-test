ac251e4b6b8d172f2b4febe1f52f44289a1c3f21
Thaman Chand <thaman.chand@invian.fi>
2021-06-03 21:28:53 +0300

    use useVisible hook, hide Cart container if click outside

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,23 +1,27 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 import Cart from '../Cart/Cart';
 import CartIcon from '../CartIcon/CartIcon';
 import Logo from '../Logo/Logo';
 
+import useVisible from '../../hooks/useVisible';
+
 import './styles.scss';
 
 const Header = () => {
-    const [toggleCart, setToggleCart] = useState(false);
-
+    const { ref, isVisible, setIsVisible } = useVisible(false);
     const handleCartToggle = () => {
-        setToggleCart(!toggleCart);
+        setIsVisible(!isVisible);
     }
 
     return (
         <div className="header">
             <Logo />
             <CartIcon onClick={handleCartToggle} />
-            {toggleCart && <Cart />}
+            {isVisible && <div ref={ref}>
+                <Cart />
+            </div>
+            }
         </div>
     )
 
