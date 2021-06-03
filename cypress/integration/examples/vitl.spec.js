f65e6f25b305958ba8fd08aeabcdb0bfcc0bf3bc
Thaman Chand <thaman.chand@invian.fi>
2021-06-03 23:02:16 +0300

    test api endpoint is working

--- cypress/integration/examples/vitl.spec.js
+++ cypress/integration/examples/vitl.spec.js
@@ -3,4 +3,14 @@ describe('Vitl cypress e2e test', () => {
         cy.visit('http://localhost:3000')
         cy.title().should('eq', 'Vitl');
     });
+
+    it('Test API endpoint returns 200 status', () => {
+        cy.request({
+            method: 'GET',
+            url: 'https://vitl-static-api.s3-eu-west-1.amazonaws.com/fe-test.json',
+        }).then(resp => resp)
+            .should((resp) => {
+                expect(resp.status).to.eq(200);
+            });
+    });
 });
