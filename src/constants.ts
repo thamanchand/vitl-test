c65266deedc51ecbfbf4fb33832ef57f2138e3ab
Thaman Chand <thaman.chand@invian.fi>
2021-06-03 11:17:02 +0300

    prepare Vitamins images constants

---
+++ src/constants.ts
@@ -0,0 +1,13 @@
+import VitaminA from "./assets/images/vitamin-A.png";
+import VitaminC from "./assets/images/vitamin-C.png";
+import VitaminD from "./assets/images/vitamin-D.png";
+import VitaminACE from "./assets/images/vitamin-ACE.png";
+import VitaminCZINC from "./assets/images/vitamin-czinc.png";
+
+export const Vitamins = [
+    VitaminA,
+    VitaminC,
+    VitaminD,
+    VitaminACE,
+    VitaminCZINC
+]
