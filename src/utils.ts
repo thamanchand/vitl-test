13d92beb18214f9009e7baa5bc715746893dc439
Thaman Chand <thaman.chand@invian.fi>
2021-06-03 11:20:29 +0300

    changed matchedNutrientsFound -> findAllMatchedNutrients. Added maor TS definition

--- src/utils.ts
+++ src/utils.ts
@@ -1,4 +1,6 @@
-import {Configs, Nutrient, Product} from './types';
+import { toast } from 'react-toastify';
+
+import { Nutrient, Product as ProductType, Product, TolerableUpperLimits } from './types';
 
 export const totalPrice = (basket: Product[]) => {
      return basket.reduce((accumulator: number, currentValue: any) => {
@@ -8,25 +10,52 @@ export const totalPrice = (basket: Product[]) => {
 
 export const ingredientsTotal = (product: Product, basket: any, id: string) => {
     const findProductNutrient = product.nutrients.find((nutrient) => nutrient.id === id);
-    return basket.map((item) => item.nutrients).reduce((accumulator, currentValue)  => {
-        if(currentValue[0].id === id) return accumulator + currentValue[0].amount + findProductNutrient?.amount;
+    return basket.map((item: Product) => item.nutrients).reduce((accumulator: number, currentValue: Nutrient[])  => {
+        if(currentValue[0].id === id && findProductNutrient?.amount) return accumulator + currentValue[0].amount + findProductNutrient?.amount;
         else return accumulator;
     }, 0);
 };
 
-export const getNutrientTUL = (tolerableUpperLimits: Configs[], id: string) => {
-    return tolerableUpperLimits.find((config: any) => config.id === id);
+export const getNutrientTUL = (tolerableUpperLimits: TolerableUpperLimits[], id: string) => {
+    return tolerableUpperLimits.find((config: TolerableUpperLimits) => config.id === id);
 }
 
-export const matchedNutrientsFound = (currentBasket: any, currentProduct: any) => {
-
+export const findAllMatchedNutrients = (currentBasket: any, currentProduct: any) => {
     // get all current product nutrient id => ['vitamin-b', 'vitamin-c'];
     const basketProductNutrientIDS = currentBasket
         .map((item: Product) => item.nutrients)
-        .map((item: Product) => item[0].id);
+        .map((item: Nutrient[]) => item[0].id);
 
     // map and find in currentBasket if nutrient.id exist in currentProductNutrientIDS
     return currentProduct.nutrients
         .map((nutrient: Nutrient) =>
             basketProductNutrientIDS.includes(nutrient.id) ? nutrient : null);
+}
+
+export const checkNutrientsTUL = (product: ProductType, basket: Nutrient, result: any, configs: TolerableUpperLimits[]) => {
+    const productNutrientsStatus: any = []; // e.g: [true, false]
+    result.forEach((item: any) => {
+        if (item?.id && item?.amount) {
+            // get TUL config of a nutrient
+            const addedProductTULConfig = getNutrientTUL(configs, item?.id);
+            // calculate total sum of current nutrient amount + basket nutrient amount
+            const nutrientSum = ingredientsTotal(product, basket, item?.id);
+
+            if (addedProductTULConfig) {
+                // If nutrientSum is >= then return true else false.
+                if (addedProductTULConfig?.amount >= nutrientSum ) {
+                    productNutrientsStatus.push(addedProductTULConfig?.amount >= nutrientSum)
+                } else {
+                    productNutrientsStatus.push(addedProductTULConfig?.amount >= nutrientSum)
+                }
+            }
+        }
+    })
+    // If all nutrients requirements passes then return true
+    // add product to basket
+    if (productNutrientsStatus.every((status: boolean) => status)) {
+        return true;
+    } else {
+        toast.error("One of protein supplement TUL level exceeded")
+    }
 }
