04748cf51d8577ad041b79239e2e68558820432e
Thaman Chand <thaman.chand@invian.fi>
2021-06-04 11:28:47 +0300

    Added JSDOC for utils

--- src/utils.ts
+++ src/utils.ts
@@ -2,11 +2,25 @@ import { toast } from 'react-toastify';
 
 import { Nutrient, Product as ProductType, Product, TolerableUpperLimits } from './types';
 
+/**
+ *
+ * @param {Object[]} basket - List of product in basket
+ * @return {number} sum - sum of product ingredients in basket
+ */
+
 export const totalPrice = (basket: Product[]) => {
      return basket.reduce((accumulator: number, currentValue: any) => {
         return accumulator + currentValue.price;
     },0);
 }
+
+/**
+ *
+ * @param {Object[]} basket - List of product in basket
+ * @param {Object[]} product - Currently added product
+ * @param {string} id - Identifier of nutrient
+ * @return {number} sum - Sum of ingredients
+ */
 
 export const ingredientsTotal = (product: Product, basket: any, id: string) => {
     const findProductNutrient = product.nutrients.find((nutrient) => nutrient.id === id);
@@ -16,9 +30,23 @@ export const ingredientsTotal = (product: Product, basket: any, id: string) => {
     }, 0);
 };
 
+/**
+ *
+ * @param {Object[]} tolerableUpperLimits - Suppliment configurations
+ * @param {string} id - Nutrient id
+ * @return {Object} {id: string, amount: number} - matched nutrient TUL configuration
+ */
+
 export const getNutrientTUL = (tolerableUpperLimits: TolerableUpperLimits[], id: string) => {
     return tolerableUpperLimits.find((config: TolerableUpperLimits) => config.id === id);
 }
+
+/**
+ *
+ * @param {Object[]} currentBasket - List of product in basket
+ * @param {Object[]} currentProduct - Currently added product
+ * @return {Object[]} [{id: string, amount: number}, null] - List of matched nutrients
+ */
 
 export const findAllMatchedNutrients = (currentBasket: any, currentProduct: any) => {
     // get all current product nutrient id => ['vitamin-b', 'vitamin-c'];
@@ -31,6 +59,15 @@ export const findAllMatchedNutrients = (currentBasket: any, currentProduct: any)
         .map((nutrient: Nutrient) =>
             basketProductNutrientIDS.includes(nutrient.id) ? nutrient : null);
 }
+
+/**
+ *
+ * @param {Object[]} product - Currently added product
+ * @param {Object[]} basket - List of products in basket
+ * @param {Object[]} result - List of matched nutrients result
+ * @param {Object[]} configs - List of ingredients configuration
+ * @return {Boolean} true | false - TUL matched nutrient
+ */
 
 export const checkNutrientsTUL = (product: ProductType, basket: Nutrient, result: any, configs: TolerableUpperLimits[]) => {
     const productNutrientsStatus: any = []; // e.g: [true, false]
